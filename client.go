c1032605679700f14f036d3d89fbc3ffbe340280
dwm@baidu <2386585789@qq.com>
2024-11-08 19:51:58 +0800

    客户端菜单显示

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int //当前客户端模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       -1,
 	}
 
 	// 链接server
@@ -30,6 +32,47 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	// 返回对象
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>>请输入合法范围内的数字<<<<<<")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for !client.menu() {
+		}
+
+		switch client.flag {
+		case 1:
+			// 公聊模式
+			fmt.Println("公聊模式...")
+			break
+		case 2:
+			// 私聊模式
+			fmt.Println("私聊模式...")
+			break
+		case 3:
+			// 更新用户名
+			fmt.Println("更新用户名...")
+			break
+		}
+	}
 }
 
 var serverIp string
@@ -51,6 +94,8 @@ func main() {
 	}
 
 	fmt.Println(">>>>>> 链接服务器成功...")
+
+	client.Run()
 
 	// 启动客户端业务
 	//select {}
